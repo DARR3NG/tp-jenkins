dae993b72cd54d0cabb8a116ca9fe967a1ffc5eb
DARR3NG <elkastaliotmane@gmail.com>
2024-01-04 17:24:05 +0100

    sc

--- POV-JAVA/src/main/java/com/example/Point/of/sale/PointOfSaleApplication.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/PointOfSaleApplication.java
@@ -1,12 +1,25 @@
 package com.example.Point.of.sale;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PointOfSaleApplication {
+
+	static Logger logger
+			= LoggerFactory.getLogger(PointOfSaleApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(PointOfSaleApplication.class, args);
 	}
+
+
+	@PostConstruct
+public void init() {
+	logger.info("Application started...");
+}
+
 }
