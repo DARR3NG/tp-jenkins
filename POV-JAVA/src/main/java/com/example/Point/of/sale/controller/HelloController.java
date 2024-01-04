ad1879e2e62d73aafd7b461f25e72c3d4741f5b2
DARR3NG <elkastaliotmane@gmail.com>
2024-01-04 18:21:08 +0100

    ffc

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello enjlsssfdd from spring boot application :)";
+        return "Hello enjlsssfdddd from spring boot application :)";
     }
 
     @GetMapping("/user")
